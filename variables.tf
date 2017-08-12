5ad29d6450cf5a6f58f5deccf71a7d020e61c54c
storbel <cpsklai@gmail.com>
2017-08-12 11:14:40 +0200

    variables usage

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "ubuntu_trusty" {
+	type = "string"
+	description =" ubuntu image on AWS AMI ID "
+	default = "ami-68bd8f75"
+}
+
+variable "redhat_rhel7" {
+	type = "string"
+	description =" redhat image on AWS AMI ID "
+	default = "ami-0e258161"
+}
